4063e27b30e7139ad28f59c1c30584bf008df509
Andres Rincon <andresf.rincong@gmail.com>
2023-05-08 10:23:43 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper('.mySwiper', {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate:15,
+        strech: 0,
+        depth: 300,
+        modifier:1,
+        slideShadows:true,
+    }
+})
